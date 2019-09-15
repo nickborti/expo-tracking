428da8895781eba0e73a986e5a7b421debc0fa0b
Sagnik Chakraborti <saaggy18@gmail.com>
2019-09-15 10:52:23 +0530

    added

---
+++ App.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Text, Button, StyleSheet, View } from 'react-native';
+import * as TaskManager from 'expo-task-manager';
+import * as Location from 'expo-location';
+
+const LOCATION_TASK_NAME = 'background-location-task';
+
+if (!TaskManager.isTaskDefined(LOCATION_TASK_NAME)) {
+  TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
+    if (error) {
+      // Error occurred - check `error.message` for more details.
+      return;
+    }
+    if (data) {
+      const { locations } = data;
+      console.log("Bacground location:: ", locations);
+      // do something with the locations captured in the background
+    }
+  });
+}
+
+export default class App extends React.Component {
+  onPress = async () => {
+    await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+      accuracy: Location.Accuracy.lowest,
+      distanceInterval:5,
+      timeInterval:2500
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Button onPress={this.onPress} title="Start tracking"/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  },
+});
